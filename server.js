298b635d596facff22ab05ec61106e8b60fb07a5
xtremerox <ramatheracer@gmail.com>
2019-09-24 19:08:05 +0530

    updated images and https

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var app = express();
-
-var server = require('http').createServer(app);
-server.listen(3000);
-console.log("Running on port 3000")
+var options = {
+    key: fs.readFileSync(__dirname+'/invalid_certs/trial.key'),
+    cert: fs.readFileSync(__dirname+'/invalid_certs/trialcert.cert')
+  };
+var server = https.createServer(options, app);
+server.listen(443);
+console.log("Running on port 443")
 app.get('/', function (req, res) {
     app.use('/static',express.static(__dirname+'/static'));
     res.sendFile(__dirname+'/public/index.html');
